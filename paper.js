8834db72fd1d755f3126a16d0f504b738416ed13
namanSeth-creator <76241731+namanSeth-creator@users.noreply.github.com>
2021-03-24 12:24:04 +0530

    Update paper.js

--- paper.js
+++ paper.js
@@ -8,7 +8,13 @@ class Paper {
         }
          this.radius = radius
             this.body = Bodies.circle(x, y, radius, options);
+	    this.radius = radius;
 	        World.add(world, this.body);
-      
-      World.add(world, this.body);
-      
+      display(){
+    ellipseMode(RADIUS);
+
+    fill("yellow");
+
+    circle(this.body.position.x, this.body.position.y, this.radius);
+  }
+    };
